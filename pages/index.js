18d3fd3d1126578094b69691d39c9bd59014bba3
Leonardo Carneiro <ccarneiro@gmail.com>
2021-01-25 23:11:06 -0300

    Conclusao do primeiro dia

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,68 @@
 import styled from 'styled-components'
+import db from '../db.json'
+import Widget from '../src/components/Widget'
+import Footer from '../src/components/Footer'
+import GitHubCorner from '../src/components/GitHubCorner'
+import QuizLogo from '../src/components/QuizLogo'
+import QuizBackground from '../src/components/QuizBackground'
 
 const Title = styled.h1`
   font-size: 50px;
   color: ${({ theme }) => theme.colors.primary};
 `
 
+export const BackgroundImage = styled.div`
+  background-image: url(${db.bg});
+  flex: 1;
+  background-size: cover;
+  background-position: center;
+`
+
+export const QuizContainer = styled.div`
+  width: 100%;
+  max-width: 350px;
+  padding-top: 45px;
+  margin: auto 10%;
+  @media screen and (max-width: 500px) {
+    margin: auto;
+    padding: 15px;
+  }
+  h1, h2, h3 {
+    font-size: 16px;
+    font-weight: 700;
+    line-height: 1;
+    margin-bottom: 0;
+  }
+  p {
+    font-size: 14px;
+    font-weight: 400;
+    line-height: 1;
+  }
+`
+
 export default function Home() {
-  return <Title>My page</Title>
+  return (
+  <QuizBackground backgroundImage={db.bg}>
+    <QuizContainer>
+      <Widget>
+        <Widget.Header>
+          <h2>Titulo</h2>
+        </Widget.Header>
+        <Widget.Content>
+          <p>
+            Testando...
+          </p>
+        </Widget.Content>
+      </Widget>
+      <Widget>
+        <Widget.Content>
+          <h2>Quiz da galera</h2>
+          <p>dasjhldkjashdklsalkdas</p>
+        </Widget.Content>
+      </Widget>
+      <Footer/>
+    </QuizContainer>
+    <GitHubCorner projectUrl="http://github.com/ccarneiro"/>
+  </QuizBackground>
+  )
 }
